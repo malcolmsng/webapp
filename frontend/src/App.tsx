9fe07387dbbbb7603024a436bbc58632ec4adb5e
Malcolm Sng <malcolmsng99@gmail.com>
2024-10-05 15:45:48 +0800

    nav bar semi done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Services from "./pages/Services";
+import Contacts from "./pages/Contacts";
 function App() {
   return (
     <>
-      <div className="text-red-500">'Hello World'</div>
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} /> */}
+          <Route path="/contact" element={<Contacts />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Navbar: React.FC = () => {
+  return <div className="text-2xl">'Home'</div>;
+};
+
+export default Navbar;
